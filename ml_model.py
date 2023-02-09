10797c372e2b28eb9019940072f6303b1cda66f4
yashodhara4884 <kmy.gowda@gmail.com>
2023-02-09 19:49:13 +0530

    update1

---
+++ ml_model.py
@@ -0,0 +1,13 @@
+print("adding ml model")
+
+def model1():
+    print("adding model 1")
+
+def model2():
+    print("adding model 2")
+
+def model():
+    print("adding model 3")
+    
+print("model 1 and 2")
+
